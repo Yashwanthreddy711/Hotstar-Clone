465c28ca2af06e66fe96696aa5fe1e022b731fec
KOMATIREDDY YASHWANTH REDDY <99392805+Yashwanthreddy711@users.noreply.github.com>
2023-02-01 17:46:50 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,32 @@
+let movies=[
+    {
+        name:'Jhansi',
+        des:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo aliquam minus repudiandae est harum excepturi laudantium autem fuga, nulla nobis laboriosam illum, possimus doloribus commodi.',
+        image:'images/slider-1.png'
+
+    },
+    {
+        name:'The Warrior',
+        des:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo aliquam minus repudiandae est harum excepturi laudantium autem fuga, nulla nobis laboriosam illum, possimus doloribus commodi.',
+        image:'images/slider-2.png'
+
+    },
+    {
+        name:'Exposed',
+        des:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo aliquam minus repudiandae est harum excepturi laudantium autem fuga, nulla nobis laboriosam illum, possimus doloribus commodi.',
+        image:'images/slider-3.png'
+
+    },
+    {
+        name:'Vikram Vedha',
+        des:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo aliquam minus repudiandae est harum excepturi laudantium autem fuga, nulla nobis laboriosam illum, possimus doloribus commodi.',
+        image:'images/slider-4.png'
+
+    },
+    {
+        name:'Bheemla Nayak',
+        des:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo aliquam minus repudiandae est harum excepturi laudantium autem fuga, nulla nobis laboriosam illum, possimus doloribus commodi.',
+        image:'images/slider-5.png'
+
+    }
+];
